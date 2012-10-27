1b125ad8794645021a30a5ecf9acb0fec3887909
NathalieHein <nl.hein@freenet.de>
2012-10-27 14:35:34 +0100

    - added additional session-layer (Model) 	!!!needs further work for locking!!! - linked Framework-Player to Gamelogic-PlayerState

---
+++ NAKJava/src/de/nordakademie/nakjava/server/internal/Sessions.java
@@ -0,0 +1,44 @@
+package de.nordakademie.nakjava.server.internal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.nordakademie.nakjava.server.shared.proxy.ServerAction;
+
+public class Sessions {
+	private static Sessions instance;
+	private Map<Long, Model> sessions = new HashMap<>();
+	private long nextSessionId = 1;
+
+	private Sessions() {
+	}
+
+	/**
+	 * {@link ActionBroker} ensures serializable transaction-handling for
+	 * {@link ServerAction}s
+	 * 
+	 * @return
+	 */
+	public static Sessions getInstance() {
+		if (instance == null) {
+			instance = new Sessions();
+		}
+		return instance;
+	}
+
+	public void addPlayer(Player player) {
+		for (Model session : sessions.values()) {
+			// this check is in this class because Model does not know that
+			// there is a session context
+			if (session.isStillRoom()) {
+				session.addPlayer(player);
+				return;
+			}
+		}
+		sessions.put(nextSessionId++, new Model(player));
+	}
+
+	public Map<Long, Model> getSessionMap() {
+		return sessions;
+	}
+}
