42de7e9e67a80973d498dd8697c08d080aafc4c9
Kai <khufenbach@aol.com>
2012-10-29 22:18:31 +0100

    Artifact clone

--- NAKJava/src/de/nordakademie/nakjava/gamelogic/shared/artifacts/ArtifactFactory.java
+++ NAKJava/src/de/nordakademie/nakjava/gamelogic/shared/artifacts/ArtifactFactory.java
@@ -19,6 +19,25 @@ public class ArtifactFactory {
 		return null;
 	}
 
+	public static <T extends Artifact> T cloneArtifact(T artifact) {
+		Class<?> clazz = artifact.getClass();
+		T clone = null;
+		try {
+			clone = (T) clazz.newInstance();
+			clone.setCount(artifact.getCount());
+
+			return clone;
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return clone;
+
+	}
+
 	public static Artifact createArtifact(Class<? extends Artifact> clazz,
 			int count) {
 		try {
