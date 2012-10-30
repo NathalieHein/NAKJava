474834c2209b03b0cc444cc452abdf9f1202a05a
Kai <khufenbach@aol.com>
2012-10-30 21:40:17 +0100

    Win Strategy Interface, to program against it.

--- NAKJava/src/de/nordakademie/nakjava/gamelogic/stateMachineEvenNewer/WinStrategy.java
+++ NAKJava/src/de/nordakademie/nakjava/gamelogic/stateMachineEvenNewer/WinStrategy.java
@@ -4,8 +4,17 @@ import java.util.Map;
 
 import de.nordakademie.nakjava.gamelogic.cards.impl.Target;
 import de.nordakademie.nakjava.gamelogic.shared.playerstate.PlayerState;
+import de.nordakademie.nakjava.gamelogic.stateMachineEvenNewer.winstrategies.RoundResult;
 
 //TODO do we allow only strategies of type "win" or also of other types: e.g. HaveAlwaysNumberOfCardsOnHandStrategy
 public interface WinStrategy {
-	public boolean hasWon(Map<Target, PlayerState> playerMap);
+	/**
+	 * Returns the RoundResult for both players. The result is in itself
+	 * consistent, which means that there are just 3 possibilities: Neutral -
+	 * Neutral, Win - Loss, Loss - Win
+	 * 
+	 * @param playerMap
+	 * @return
+	 */
+	public Map<Target, RoundResult> hasWon(Map<Target, PlayerState> playerMap);
 }
