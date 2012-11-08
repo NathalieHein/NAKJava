5a57b883140d80b9850034d4ccb000c9050392f4
NathalieHein <nl.hein@freenet.de>
2012-11-08 11:42:28 +0000

    added additional field to EditDeckSpecificInformation in PlayerModel visible for Client: currentPartOfDeckName

--- NAKJava/src/de/nordakademie/nakjava/server/shared/serial/stateSpecificInfos/EditDeckSpecificInformation.java
+++ NAKJava/src/de/nordakademie/nakjava/server/shared/serial/stateSpecificInfos/EditDeckSpecificInformation.java
@@ -4,13 +4,20 @@ import java.util.Map;
 
 public class EditDeckSpecificInformation implements StateSpecificInformation {
 	private Map<String, Boolean> chosenCards;
+	private String currentPartOfDeckName;
 
-	public EditDeckSpecificInformation(Map<String, Boolean> chosenCards) {
+	public EditDeckSpecificInformation(Map<String, Boolean> chosenCards,
+			String currentPartOfDeckName) {
 		this.chosenCards = chosenCards;
+		this.currentPartOfDeckName = currentPartOfDeckName;
 	}
 
 	public Map<String, Boolean> getChosenCards() {
 		return chosenCards;
+	}
+
+	public String getCurrentPartOfDeckName() {
+		return currentPartOfDeckName;
 	}
 
 }
