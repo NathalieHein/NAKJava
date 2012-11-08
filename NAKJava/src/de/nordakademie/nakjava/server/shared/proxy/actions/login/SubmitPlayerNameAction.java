e8ac62614ba247991edafd9c4dff5afd2d7cc53a
NathalieHein <nl.hein@freenet.de>
2012-11-08 21:00:21 +0000

    - first actions for login

--- NAKJava/src/de/nordakademie/nakjava/server/shared/proxy/actions/login/SubmitPlayerNameAction.java
+++ NAKJava/src/de/nordakademie/nakjava/server/shared/proxy/actions/login/SubmitPlayerNameAction.java
@@ -2,8 +2,10 @@ package de.nordakademie.nakjava.server.shared.proxy.actions.login;
 
 import java.rmi.RemoteException;
 
+import de.nordakademie.nakjava.gamelogic.shared.playerstate.PlayerState;
 import de.nordakademie.nakjava.gamelogic.stateMachineEvenNewer.StateMachine;
 import de.nordakademie.nakjava.server.internal.Session;
+import de.nordakademie.nakjava.server.internal.model.LoginSpecificModel;
 import de.nordakademie.nakjava.server.shared.proxy.ActionAbstractImpl;
 import de.nordakademie.nakjava.server.shared.proxy.ServerAction;
 import de.nordakademie.nakjava.server.shared.serial.ActionContext;
@@ -23,6 +25,13 @@ public class SubmitPlayerNameAction extends ActionContext {
 				if (!session.isActionInvokerCurrentPlayer()) {
 					session.getModel().changeSelfAndOpponent();
 				}
+				PlayerState self = session.getModel().getSelf();
+				LoginSpecificModel model = (LoginSpecificModel) self
+						.getStateSpecificModel();
+				session.getActionInvoker()
+						.setName(model.getCurrentPartOfName());
+				self.setStateSpecificModel(null);
+				// TODO getPlayersDecks
 				StateMachine.getInstance().run(session.getModel());
 			}
 		};
