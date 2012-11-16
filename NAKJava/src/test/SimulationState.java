6552e52ddacc7a237780c88bf42e1fe4aa0a84e8
Kai <khufenbach@aol.com>
2012-11-16 15:24:34 +0100

    SimulationState

---
+++ NAKJava/src/test/SimulationState.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.List;
+
+import de.nordakademie.nakjava.gamelogic.shared.cards.CardInformation;
+import de.nordakademie.nakjava.server.shared.serial.PlayerState;
+
+public class SimulationState {
+
+	private List<CardInformation> cards;
+
+	public PlayerState getSimulatedState(CardInformation card, int i) {
+		return null;
+	}
+}
