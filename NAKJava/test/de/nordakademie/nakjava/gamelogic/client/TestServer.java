b3d9be2e157eafb5e3a8ca1e5b121918ec61bae9
NathalieHein <nl.hein@freenet.de>
2012-11-18 20:03:35 +0000

    - ServerTest

--- NAKJava/test/de/nordakademie/nakjava/gamelogic/client/TestServer.java
+++ NAKJava/test/de/nordakademie/nakjava/gamelogic/client/TestServer.java
@@ -7,7 +7,16 @@ import junit.framework.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.nordakademie.nakjava.gamelogic.stateMachineEvenNewer.states.State;
+import de.nordakademie.nakjava.generated.VisibleModelFields;
 import de.nordakademie.nakjava.server.shared.proxy.CheckInImpl;
+import de.nordakademie.nakjava.server.shared.proxy.actions.LeaveGameAction;
+import de.nordakademie.nakjava.server.shared.proxy.actions.cardActions.PlayCardAction;
+import de.nordakademie.nakjava.server.shared.proxy.actions.editdeck.DiscardDeckEditAction;
+import de.nordakademie.nakjava.server.shared.proxy.actions.login.TypePlayerNameAction;
+import de.nordakademie.nakjava.server.shared.proxy.actions.settingupgame.CreateNewDeckAction;
+import de.nordakademie.nakjava.server.shared.proxy.actions.settingupgame.FinishConfiguringAction;
+import de.nordakademie.nakjava.server.shared.serial.ActionContext;
 
 public class TestServer {
 
@@ -50,5 +59,164 @@ public class TestServer {
 		Assert.assertTrue(bot1.init);
 		Assert.assertTrue(bot2.init);
 
+	}
+
+	@Test
+	public void testActions() {
+		State currentState;
+		currentState = getState(client1);
+		Assert.assertEquals(currentState, State.LOGIN);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof TypePlayerNameAction) {
+				if (((TypePlayerNameAction) action).getKey() == 'a') {
+					action.perform();
+				}
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(getState(client1), State.LOGIN);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof LeaveGameAction) {
+				action.perform();
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(getState(client1), State.CONFIGUREGAME);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof CreateNewDeckAction) {
+				action.perform();
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(getState(client1), State.EDITDECK);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof DiscardDeckEditAction) {
+				action.perform();
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(getState(client1), State.CONFIGUREGAME);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof FinishConfiguringAction) {
+				action.perform();
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(getState(client1), State.READYTOSTARTSTATE);
+
+		Assert.assertEquals(currentState, State.LOGIN);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof TypePlayerNameAction) {
+				if (((TypePlayerNameAction) action).getKey() == 'b') {
+					action.perform();
+				}
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(getState(client1), State.LOGIN);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof LeaveGameAction) {
+				action.perform();
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(getState(client1), State.CONFIGUREGAME);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof CreateNewDeckAction) {
+				action.perform();
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(getState(client1), State.EDITDECK);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof DiscardDeckEditAction) {
+				action.perform();
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(getState(client1), State.CONFIGUREGAME);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof FinishConfiguringAction) {
+				action.perform();
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(getState(client2), State.STOP);
+
+		Assert.assertEquals(getState(client1), State.PLAYCARDSTATE);
+		for (ActionContext action : client1.state.getActions()) {
+			if (action instanceof PlayCardAction) {
+				action.perform();
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Assert.assertTrue(getState(client1) == State.ADJUSTCARDHANDSTATE
+				|| getState(client1) == State.STOP);
+		Assert.assertTrue(getState(client2) == State.PLAYCARDSTATE
+				|| getState(client2) == State.STOP);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private State getState(TestClient client) {
+		return VisibleModelFields.PLAYERSTATE_STATE_SELF.getValue(client.state
+				.getModel().getGenericTransfer());
 	}
 }
